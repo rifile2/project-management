51cf4b5077bd2e31f238c2b14394ead6e3b157a3
Ishu Rajput <ishu84205@gmail.com>
2026-05-12 00:41:59 +0530

    sec

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -76,10 +76,105 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
+
+// Inngest Function to save workspace data to a database
+
+const syncWorkspaceCreation = inngest.createFunction(
+  { id: 'sync-workspace-from-clerk' },
+
+  { event: 'clerk/organization.created' },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.workspace.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        slug: data.slug,
+        ownerId: data.created_by,
+        image_url: data.image_url,
+      },
+    })
+    await prisma.workspaceMember.create({
+      data:{
+        userID:data.created_by,
+        workspaceId:data.id,
+        role:"ADMIN"
+      }
+    })
+  }
+);
+
+// Inngest Function to update workspace data in database
+
+const syncWorkspaceUpdation = inngest.createFunction(
+  { id: 'update-workspace-from-clerk' },
+
+  { event: 'clerk/organization.updated' },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.workspace.update({
+      where: {
+        id: data.id,
+      },
+
+      data: {
+        name: data.name,
+        slug: data.slug,
+        image_url: data.image_url,
+      },
+    });
+  }
+);
+// Inngest Function to delete workspace data from database
+
+const syncWorkspaceDeletion = inngest.createFunction(
+  { id: 'delete-workspace-from-clerk' },
+
+  { event: 'clerk/organization.deleted' },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.workspace.delete({
+      where: {
+        id: data.id,
+      },
+    });
+  }
+);
+
+// Inngest Function to save workspace member data to a database
+
+const syncWorkspaceMemberCreation = inngest.createFunction(
+  { id: 'sync-workspace-member-from-clerk' },
+
+  { event: 'clerk/organizationInvitation.accepted' },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.user_id,
+        workspaceId: data.organization_id,
+        role: String(data.role_name).toUpperCase(),
+      },
+    });
+  }
+);
 // Export all functions
 
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
+  syncWorkspaceCreation,
+  syncWorkspaceMemberCreation,
+  syncWorkspaceUpdation,
+  syncWorkspaceDeletion
+
 ];
